55974ee915256df83f13b45802840c88827a1e57
Ishwar Sarade <89654045+heyIshwar@users.noreply.github.com>
2024-04-22 15:42:56 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,41 @@
+# Install Visual Studio Code
+winget install --id Microsoft.VisualStudioCode
+
+# Install Git
+winget install --id Git.Git
+
+# Install Node.js and Node Version Manager (nvm)
+winget install --id Nodejs.Nodejs
+winget install --id RavenPowers.NodeVersionManager
+
+# Install PowerToys
+winget install --id Microsoft.PowerToys
+
+# Install Windows Terminal
+winget install --id Microsoft.WindowsTerminal
+
+# Install Postman
+winget install --id Postman.Postman
+
+# Install Docker Desktop
+winget install --id Docker.DockerDesktop
+
+# Install Google Chrome
+winget install --id Google.Chrome
+
+# Install NoSQL Booster
+winget install --id NoSQLBooster.NoSQLBooster
+
+# Install MySQL Workbench
+winget install --id Oracle.MySQL.Workbench
+
+# Install VSCode extensions
+code --install-extension ms-vscode.PowerShell
+code --install-extension formulahendry.code-runner
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension redhat.vscode-yaml
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension githubsecure.githubsecure
+code --install-extension EditorConfig.EditorConfig
